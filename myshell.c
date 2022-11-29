aaf6e198e239406431720c793c5c5393d6503b75
okadali <okadali@hotmail.com>
2022-11-29 10:04:30 +0300

    basic functionality

--- myshell.c
+++ myshell.c
@@ -3,15 +3,48 @@
 #include <stdlib.h>
 #include "sys/wait.h"
 #include "string.h"
+#include <readline/readline.h>
+#include <dirent.h> 
 
 int main(int argc, char *argv[]) {
-    char input[100];
-    while (strcmp(input,"exit")) {
-        scanf("%s",input);
+    char *input;
+    while (1) {
+        input = readline("MYSHELL>>");
+        if(strlen(input) == 0) {
+            continue;
+        }
+        
 
-        if (strcmp(input,"clear") == 0) {
+        int ix = 0;
+        char *inputs[80];
+        char *token = strtok(input," ");
+        while( token != NULL ) {
+            inputs[ix] = token;
+            ix++;
+            token = strtok(NULL," ");
+        }
+        
+        if(strcmp(inputs[0],"exit") == 0) {
+            return 1;
+        }
+        else if(strcmp(inputs[0],"clear") == 0) {
             system("clear");
         }
+        else if(strcmp(inputs[0],"cat") == 0) {
+            printf("cat:%s\n",inputs[1]);
+        }
+        else if(strcmp(inputs[0],"ls") == 0) {
+            DIR *d;
+            struct dirent *dir;
+            d = opendir(".");
+            if (d) {
+                while ((dir = readdir(d)) != NULL) {
+                    printf("%s\n", dir->d_name);
+                }
+                closedir(d);
+            }
+        }
+        
         
     }
     
